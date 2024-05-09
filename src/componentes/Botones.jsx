83200916678afcbd169c5d68a0343f55d05a33b9
Natalia Lozano <acuarionat2004@gmail.com>
2024-05-09 19:23:46 -0400

    tercer componente

--- src/componentes/Botones.jsx
+++ src/componentes/Botones.jsx
@@ -3,7 +3,11 @@ import "./Botones.css";
 
 const Botones = () => {
   return (
-    <div>Botones</div>
+    <div className='Botones'>
+        <button className='follow'>Follow</button>
+        <button className='send'><img src={"./public/Vector.svg"} alt={"enviar"} /></button>
+
+    </div>
   )
 }
 
